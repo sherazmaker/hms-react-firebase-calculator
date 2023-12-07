0a9b271d63277801162c1e57c70bac195e6acff4
sherazmaker <sheraz.q@hashmakersol.net>
2023-12-07 14:32:19 +0500

    auth with firebase

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <h1 style={{ color: "white" }}>First Name, Last Name, Logout</h1>
+        </div>
+    );
+};
+
+export default Header;
